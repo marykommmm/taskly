40c0d6f3f2ac8cd422d12eced0f58b5450e0e29c
mariakomarnicka4@outlook.com <mariia.komarnytskaa@gmail.com>
2025-01-01 15:45:47 +0200

    feat commit

--- backend/src/middleware/authMiddleware.js
+++ backend/src/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ export const protect = asyncHandler(async (req, res, next) => {
 
     if (!token) {
       // 401 Unauthorized
-      res.status(401).json({ message: "Not authorized, please login!" });
+      return res.status(401).json({ message: "Not authorized, please login!" });
     }
 
     // verify the token
@@ -20,7 +20,7 @@ export const protect = asyncHandler(async (req, res, next) => {
 
     // check if user exists
     if (!user) {
-      res.status(404).json({ message: "User not found!" });
+      return res.status(404).json({ message: "User not found!" });
     }
 
     // set user details in the request object
@@ -29,7 +29,7 @@ export const protect = asyncHandler(async (req, res, next) => {
     next();
   } catch (error) {
     // 401 Unauthorized
-    res.status(401).json({ message: "Not authorized, token failed!" });
+    return res.status(401).json({ message: "Not authorized, token failed!" });
   }
 });
 
@@ -37,11 +37,10 @@ export const protect = asyncHandler(async (req, res, next) => {
 export const adminMiddleware = asyncHandler(async (req, res, next) => {
   if (req.user && req.user.role === "admin") {
     // if user is admin, move to the next middleware/controller
-    next();
-    return;
+    return next();
   }
   // if not admin, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Only admins can do this!" });
+  return res.status(403).json({ message: "Only admins can do this!" });
 });
 
 export const creatorMiddleware = asyncHandler(async (req, res, next) => {
@@ -50,20 +49,18 @@ export const creatorMiddleware = asyncHandler(async (req, res, next) => {
     (req.user && req.user.role === "admin")
   ) {
     // if user is creator, move to the next middleware/controller
-    next();
-    return;
+    return next();
   }
   // if not creator, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Only creators can do this!" });
+  return res.status(403).json({ message: "Only creators can do this!" });
 });
 
 // verified middleware
 export const verifiedMiddleware = asyncHandler(async (req, res, next) => {
   if (req.user && req.user.isVerified) {
     // if user is verified, move to the next middleware/controller
-    next();
-    return;
+    return next();
   }
   // if not verified, send 403 Forbidden --> terminate the request
-  res.status(403).json({ message: "Please verify your email address!" });
+  return res.status(403).json({ message: "Please verify your email address!" });
 });
